fd14d09c0fe0f79d4ed85497d503f7d0fc1c2ce1
Munikrishnappa <AG32841@ad.wellpoint.com>
2019-10-31 16:32:15 +0530

    Updated the service layer code

--- voters-app-with-swagger/src/main/java/org/manjunath/springboot/votersappwithswagger/service/VoterService.java
+++ voters-app-with-swagger/src/main/java/org/manjunath/springboot/votersappwithswagger/service/VoterService.java
@@ -1,5 +1,17 @@
 package org.manjunath.springboot.votersappwithswagger.service;
 
-public interface VoterService {
+import java.util.List;
 
+import org.manjunath.springboot.votersappwithswagger.model.Voter;
+
+public interface VoterService {
+	
+	public List<Voter> getAllVoters();
+	public Voter getVoterById(int id);
+	public Voter getVoterById(String voterId);
+	public Voter addVoter(Voter voter);
+	public Voter updateVoter(Voter voter);
+	public void deleteVoterById(int id);
+	public void deleteVoterById(String voterId);
+	
 }
--- voters-app-with-swagger/src/main/java/org/manjunath/springboot/votersappwithswagger/service/VoterServiceImpl.java
+++ voters-app-with-swagger/src/main/java/org/manjunath/springboot/votersappwithswagger/service/VoterServiceImpl.java
@@ -1,8 +1,46 @@
 package org.manjunath.springboot.votersappwithswagger.service;
 
+import java.util.List;
+
+import org.manjunath.springboot.votersappwithswagger.model.Voter;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VoterServiceImpl implements VoterService {
+
+	@Override
+	public List<Voter> getAllVoters() {
+		return null;
+	}
+
+	@Override
+	public Voter getVoterById(int id) {
+		return null;
+	}
+
+	@Override
+	public Voter getVoterById(String voterId) {
+		return null;
+	}
+
+	@Override
+	public Voter addVoter(Voter voter) {
+		return null;
+	}
+
+	@Override
+	public Voter updateVoter(Voter voter) {
+		return null;
+	}
+
+	@Override
+	public void deleteVoterById(int id) {
+		
+	}
+
+	@Override
+	public void deleteVoterById(String voterId) {
+		
+	}
 
 }
