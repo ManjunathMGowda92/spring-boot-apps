1a6b894c74129aee346b7ee846f7cc2603358199
Munikrishnappa <AG32841@ad.wellpoint.com>
2019-11-01 19:02:04 +0530

    Exception handling for DataIntegrityViolationException and added columns for Models

--- voters-app-with-swagger/src/main/java/org/manjunath/springboot/votersappwithswagger/exceptionhandling/CustomErrorCodes.java
+++ voters-app-with-swagger/src/main/java/org/manjunath/springboot/votersappwithswagger/exceptionhandling/CustomErrorCodes.java
@@ -5,8 +5,11 @@ public interface CustomErrorCodes {
 	public static final String VOTER_NOT_FOUND_CODE = "VOTER_001";
 	public static final String VOTER_NOT_FOUND_MSG = "Specified Voter Not Found";
 	
+	public static final String VOTER_CONSTARINT_EXCEPTION_CODE = "VOTER_002";
+	public static final String VOTER_CONSTARINT_EXCEPTION_MSG = "Unique Constraint Voilation Exception Occurred.";
+	
 	public static final String VOTER_GENERIC_EXCEPTION_CODE = "VOTER_010";
-	public static final String VOTER_GENERIC_EXCEPTION_MSG = "An Exception Occurred. Please Contact Administrator";
+	public static final String VOTER_GENERIC_EXCEPTION_MSG = "An Exception Occurred. Please Contact Administrator.";
 	
 	public static final String VOTER_NULL_EXCEPTION_CODE = "VOTER_011";
 	public static final String VOTER_NULL_EXCEPTION_MSG = "NullPointerException Occurred.";
