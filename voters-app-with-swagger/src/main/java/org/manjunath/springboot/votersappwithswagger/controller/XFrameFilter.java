fd27fe9c61b8b9b52aa1b33a0f624942681e6b93
Munikrishnappa <AG32841@ad.wellpoint.com>
2019-11-01 19:35:23 +0530

    Added Xframe filter

---
+++ voters-app-with-swagger/src/main/java/org/manjunath/springboot/votersappwithswagger/controller/XFrameFilter.java
@@ -0,0 +1,36 @@
+package org.manjunath.springboot.votersappwithswagger.controller;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class XFrameFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if (request instanceof HttpServletRequest && response instanceof HttpServletResponse) {
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+			httpResponse.setHeader("X-XSS-Protection", "1; mode=block");
+			httpResponse.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+			httpResponse.setHeader("X-Content-Type-Options", "nosniff");
+			httpResponse.setHeader("Cache-control", "no-store, no-cache");
+			httpResponse.setHeader("X-Frame-Options", "SAMEORIGIN");
+			httpResponse.setHeader("Set-Cookie",
+					"XSRF-TOKEN=NDKDdfdsfkldsfNd3SZAJfwLsTl5WUgOkE; Path=/; Secure;HttpOnly");
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+}
