17a59a5e047f4d6c4047cc5449207a17a21552f0
Munikrishnappa <AG32841@ad.wellpoint.com>
2019-11-08 17:06:26 +0530

    Added swagger documentation for get All Voters

--- voters-app-with-swagger/src/main/java/org/manjunath/springboot/votersappwithswagger/controller/VoterController.java
+++ voters-app-with-swagger/src/main/java/org/manjunath/springboot/votersappwithswagger/controller/VoterController.java
@@ -14,41 +14,59 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/api")
+@Api(tags = { "Voter Application Controller" })
 public class VoterController {
-	
+
 	@Autowired
 	private VoterService service;
-	
+
+	@ApiOperation(value = "View a list of available Voters", 
+			consumes = "application/json", 
+			produces = "application/json", 
+			response = List.class, 
+			httpMethod = "GET", 
+			notes = "Rest Controller end point to fetch all the voters present in the database")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	})
 	@GetMapping("/voters")
 	public List<Voter> getAllVoters() {
 		return service.getAllVoters();
 	}
-	
+
 	@GetMapping("/voters/{id}")
 	public Voter getVoterById(@PathVariable("id") int id) {
 		return service.getVoterById(id);
 	}
-	
+
 	@GetMapping("/voters/voter/{voterId}")
 	public Voter getVoterByVoterId(@PathVariable("voterId") String voterId) {
 		return null;
 	}
-	
+
 	@PostMapping("/voters")
 	public Voter addVoter(@RequestBody Voter voter) {
 		voter.setId(0);
 		return service.addVoter(voter);
 	}
-	
+
 	@PutMapping("/voters/{id}")
 	public Voter updateVoter(@RequestBody Voter voter, @PathVariable("id") int id) {
 		return service.updateVoter(voter);
 	}
-	
+
 	@DeleteMapping("/voters/{id}")
-	public Voter deleteVoterById(@PathVariable("id") int id){
+	public Voter deleteVoterById(@PathVariable("id") int id) {
 		return service.deleteVoterById(id);
 	}
 }
