b712667ee7cfb2966241af37c84d3bc093accc70
Munikrishnappa <AG32841@ad.wellpoint.com>
2019-11-19 21:44:33 +0530

    Basic Structure

---
+++ basic-voter-api/src/main/java/org/manjunath/basicvoterapi/services/VoterServiceImpl.java
@@ -0,0 +1,34 @@
+package org.manjunath.basicvoterapi.services;
+
+import java.util.List;
+
+import org.manjunath.basicvoterapi.model.Voter;
+
+public class VoterServiceImpl implements VoterService {
+
+	@Override
+	public List<Voter> getVotersList() {
+		return null;
+	}
+
+	@Override
+	public Voter getVoterById(int id) {
+		return null;
+	}
+
+	@Override
+	public Voter addVoter(Voter voter) {
+		return null;
+	}
+
+	@Override
+	public Voter updateVoter(Voter voter) {
+		return null;
+	}
+
+	@Override
+	public Voter deleteVoter(int id) {
+		return null;
+	}
+
+}
