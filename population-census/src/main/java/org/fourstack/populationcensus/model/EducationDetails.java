1853025595ae65aaa9275c5144b87c6f22dc89d8
Hm <Manjunath_Hm@Dellteam.com>
2020-03-04 11:27:48 +0530

    Mapped Education Details and Person with OnetoOne

--- population-census/src/main/java/org/fourstack/populationcensus/model/EducationDetails.java
+++ population-census/src/main/java/org/fourstack/populationcensus/model/EducationDetails.java
@@ -1,8 +1,26 @@
 package org.fourstack.populationcensus.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "education_details")
 public class EducationDetails implements Serializable{
 
 	/**
@@ -10,12 +28,44 @@ public class EducationDetails implements Serializable{
 	 */
 	private static final long serialVersionUID = -7283675211201911008L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private long id;
+	
+	@Column(name = "education_level")
+	@JsonProperty("level_of_education")
 	private String educationLevel;
+	
+	@Column(name = "education_status")
+	@JsonProperty("status_of_eduaction")
 	private String educationStatus;
+	
+	@Column(name = "highest_education")
+	@JsonProperty("highest_education")
 	private String highestEducation;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "education_detail_id")
+	@JsonProperty("education_level_details")
 	private List<EducationLevelDetails> educationLevelDetails;
 	
+	/*
+	 * Below column is for Bi-Directional mapping of Hibernate. This column maps
+	 * back to the person and informs the hibernate saying mapping will be taken
+	 * care by the "educationDetails" field in the Person column. So it avoids multiple table
+	 * creation for mapping the PERSON and EDUCATION_DETAILS tables
+	 */
+	@OneToOne(mappedBy = "educationDetails", cascade = CascadeType.ALL)
+	private Person person;
+	
+	@CreationTimestamp
+	@Column(name = "created_date", nullable = false, updatable = false)
+	private LocalDateTime createdDateTime;
+	
+	@CreationTimestamp
+	@Column(name = "updated_date", nullable = false, updatable = true)
+	private LocalDateTime updatedDateTime;
 	
 	public EducationDetails() {
 	}
--- population-census/src/main/java/org/fourstack/populationcensus/model/EducationLevelDetails.java
+++ population-census/src/main/java/org/fourstack/populationcensus/model/EducationLevelDetails.java
@@ -3,6 +3,20 @@ package org.fourstack.populationcensus.model;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "education_level_details")
 public class EducationLevelDetails implements Serializable {
 
 	/**
@@ -10,22 +24,54 @@ public class EducationLevelDetails implements Serializable {
 	 */
 	private static final long serialVersionUID = 1679777833999739416L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
+	
+	@Column(name = "institute_name")
+	@JsonProperty("name_of_institute")
 	private String instituteName;
+	
+	@Column(name = "highest_education_in_institute")
+	@JsonProperty("highest_education_in_institute")
 	private String highestEducationInInstitute;
+	
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	@JsonFormat(pattern = "yyyy/MM/dd")
+	@Column(name = "start_year")
+	@JsonProperty("start_year")
 	private LocalDate startYear;
+	
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	@JsonFormat(pattern = "yyyy/MM/dd")
+	@Column(name = "end_year")
+	@JsonProperty("end_year")
 	private LocalDate endYear;
+	
+	@Column(name = "course_completion_status")
+	@JsonProperty("is_course_completed")
 	private boolean courseCompletionStatus;
 	
 	public EducationLevelDetails() {
 	}
-	
-	public EducationLevelDetails(String instituteName, String highestEducationInInstitute, LocalDate startYear,
+		
+	public EducationLevelDetails(long id, String instituteName, String highestEducationInInstitute, LocalDate startYear,
 			LocalDate endYear, boolean courseCompletionStatus) {
+		this.id = id;
 		this.instituteName = instituteName;
 		this.highestEducationInInstitute = highestEducationInInstitute;
 		this.startYear = startYear;
 		this.endYear = endYear;
 		this.courseCompletionStatus = courseCompletionStatus;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public String getInstituteName() {
