86d2d96fc0ab65785f07647e889e6c66f25bed14
ManjunathMGowda92 <manjunathcs052@gmail.com>
2020-03-11 19:39:02 +0530

    Added dependency for TestNG and created Base class

---
+++ population-census/src/test/java/org/fourstack/populationcensus/tests/PopulationCensusBaseTest.java
@@ -0,0 +1,10 @@
+package org.fourstack.populationcensus.tests;
+
+import org.fourstack.populationcensus.PopulationCensusApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@SpringBootTest(classes = PopulationCensusApplication.class)
+public class PopulationCensusBaseTest extends AbstractTestNGSpringContextTests{
+
+}
