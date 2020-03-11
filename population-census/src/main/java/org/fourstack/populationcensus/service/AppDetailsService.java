f2886aad2124a7087c7436cf70e1a00df43b008c
ManjunathMGowda92 <manjunathcs052@gmail.com>
2020-03-11 18:40:02 +0530

    Updated code to fetch the endpoints details

--- population-census/src/main/java/org/fourstack/populationcensus/service/AppDetailsService.java
+++ population-census/src/main/java/org/fourstack/populationcensus/service/AppDetailsService.java
@@ -1,6 +1,10 @@
 package org.fourstack.populationcensus.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.fourstack.populationcensus.model.CensusApplicationModel;
+import org.fourstack.populationcensus.model.EndpointsDescription;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -23,5 +27,16 @@ public class AppDetailsService {
 		appModel.setApplicationDescription(applicationDescription);
 		
 		return appModel;
+	}
+	
+	public List<EndpointsDescription> getEndPointsDetails() {
+		List<EndpointsDescription> endPoints = new ArrayList<>();
+
+		endPoints.add(new EndpointsDescription("@GET", "/census/api/persons", "To fetch list of all Persons"));
+		endPoints.add(new EndpointsDescription("@GET", "/census/api/persons/{id}", "To fetch particular Person based on the id"));
+		endPoints.add(new EndpointsDescription("@POST", "/census/api/persons", "To save a new Person to the database"));
+		endPoints.add(new EndpointsDescription("@PUT", "/census/api/persons/{id}", "To update an existing Person based on the id"));
+		
+		return endPoints;
 	}
 }
